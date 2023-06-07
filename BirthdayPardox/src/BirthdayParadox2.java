ca7fea7a9e4b0b086e5158dcfec1fb9ccd0f8830
Abdulla Mushnath <abd.mushnath@gmail.com>
2023-06-07 21:01:37 +0530

    Wrote a new code with different name to get tr/fls

---
+++ BirthdayPardox/src/BirthdayParadox2.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class BirthdayParadox2 {
+    public static void main(String[] args){
+        int n = 5; //people in the room
+        boolean haveSameBirthday = runTest(n);
+
+        if (haveSameBirthday){
+            System.out.println("Two people in a room of " + n + " have the same birthday!");
+        }
+        else
+        {
+            System.out.println("No Two people in a room of " + n + " have the same birthday!");
+        }
+    }
+
+    private static boolean runTest(int n) {
+        int[] birthdays = new int[365];
+
+        Random rand = new Random();
+        for(int i = 0; i<n;i++){
+            int birthday = rand.nextInt(365);
+            if(birthdays[birthday]>0){
+                return true;
+            }
+            birthdays[birthday]++;
+        }
+        return false;
+        
+    }
+}
